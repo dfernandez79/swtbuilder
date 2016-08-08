13584d742b7db4de9879398d43d483c9a271248f
Diego Fernandez <diego.fernandez@mulesoft.com>
2016-08-08 13:33:56 -0300

    Fix of serialVersionUID warnings in Eclipse

--- src/main/java/swtbuilder/DuplicateIdException.java
+++ src/main/java/swtbuilder/DuplicateIdException.java
@@ -1,4 +1,5 @@
 package swtbuilder;
 
 public class DuplicateIdException extends RuntimeException {
+    private static final long serialVersionUID = -4095210890460582247L;
 }
--- src/main/java/swtbuilder/UnknownControlException.java
+++ src/main/java/swtbuilder/UnknownControlException.java
@@ -1,4 +1,5 @@
 package swtbuilder;
 
 public class UnknownControlException extends RuntimeException {
+    private static final long serialVersionUID = -8043002827450983929L;
 }
